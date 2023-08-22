d429c4a9f32c71cd4a2ea4308e23ba006b8502bc
Raj chaturvedi <116450136+Playstationgaming@users.noreply.github.com>
2023-08-22 18:43:22 +0530

    Add files via upload

---
+++ new69.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int size;
+    cin>>size;
+    int arr[size];
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+        }
+        sort(arr,arr+size);
+        bool ans=false;
+      for(int i=0;i<size-2;i++){
+        int l=0;
+        int r=size-1;
+        while(arr[l]<arr[r]){
+            int sum=arr[i]+arr[l]+arr[r];
+            if(sum==0){
+                ans=true;
+                break;
+            }
+            else if(sum<0){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+      }
+      if(ans==true){
+        cout<<"Output: 1";
+      }
+      else{
+        cout<<"Output 0";
+      }
+
+}
